ae02db94910678897ee98c99c983f169b658b0d0
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-02 18:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.getElementById("drag")
+.addEventListener("ondragstart",(event)=>{
+	console.log("Dragging started",event)
+})
+document.getElementById("div1")
+.addEventListener("ondrop",(event)=>{
+	console.log("Drop started",event)
+})
+document.getElementById("div1").addEventListener("ondragover",event=>{
+console.log("Dragging over",event)	
+})
 //your code here
 function allowDrop(event) {
 	ev.preventDefault();
